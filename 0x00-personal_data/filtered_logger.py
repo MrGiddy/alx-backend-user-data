13497cdaa390ddca19039609d294d02b8edf3ac2
Matumbai Binale <binalematumbai@gmail.com>
2024-08-29 21:35:33 +0300

    (task 1): Create RedactingFormatter class for log record(s)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Handling backend user data"""
+import logging
 import re
 from typing import List
 
@@ -20,3 +21,21 @@ def filter_datum(
     for field in fields:
         message = replace(field, redaction, message, separator)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Instantiate a RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """reduct values in incoming log record(s)"""
+        msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
