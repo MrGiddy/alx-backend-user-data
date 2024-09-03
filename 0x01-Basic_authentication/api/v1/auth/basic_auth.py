d83984e009b238367f819fd031a5b3d90e3a2c68
Matumbai Binale <binalematumbai@gmail.com>
2024-09-04 02:48:41 +0300

    (task 8): Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """BasicAuth module"""
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -11,6 +13,13 @@ class BasicAuth(Auth):
         """extracts base64-encoded username:password combo from auth header"""
         if authorization_header and isinstance(authorization_header, str):
             if authorization_header.startswith('Basic '):
-                encoded_uname_pwd = authorization_header.split('Basic ', 1)[1]
-                return encoded_uname_pwd
-        return None
+                return authorization_header.split('Basic ', 1)[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """decodes a base64-encoded auth header value"""
+        if isinstance(base64_authorization_header, str):
+            try:
+                byte_string = base64.b64decode(base64_authorization_header)
+                return byte_string.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
