238890a2da7d335f4f9fd97390b47aaf66eb257b
Matumbai Binale <binalematumbai@gmail.com>
2024-09-03 04:03:45 +0300

    (task 3): Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Auth module"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require authentication method"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user method"""
+        return None
