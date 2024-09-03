f98efc7f59cc8de126ddb4403343be5b89e17558
Matumbai Binale <binalematumbai@gmail.com>
2024-09-04 02:25:27 +0300

    (task 7): Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """extracts base64-encoded username:password combo from auth header"""
+        if authorization_header and isinstance(authorization_header, str):
+            if authorization_header.startswith('Basic '):
+                encoded_uname_pwd = authorization_header.split('Basic ', 1)[1]
+                return encoded_uname_pwd
+        return None
