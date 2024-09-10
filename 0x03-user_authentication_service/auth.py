550373941f86f010a7d32b040662ceb54f5f2b09
Matumbai Binale <binalematumbai@gmail.com>
2024-09-11 02:19:17 +0300

    (task 8, 9): Validate user credentials, generate uuids

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Authentication module """
+from uuid import uuid4
 import bcrypt
 from db import DB
 from user import User
@@ -9,6 +10,11 @@ from sqlalchemy.orm.exc import NoResultFound
 def _hash_password(password: str) -> bytes:
     """ returns an encrypted password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """returns a stringified uuid"""
+    return str(uuid4())
 
 
 class Auth:
@@ -29,3 +35,12 @@ class Auth:
         hashed_pwd = _hash_password(password)
         user = self._db.add_user(email, hashed_pwd)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validate a user's login details"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
