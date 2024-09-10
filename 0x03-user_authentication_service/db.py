63f0c050cbd114d5a0d2887e45243ec25aaaecb2
Matumbai Binale <binalematumbai@gmail.com>
2024-09-10 04:21:47 +0300

    (task 2): Find a user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base
 from user import User
 
@@ -35,4 +36,19 @@ class DB():
         sesh.commit()
 
         sesh.refresh(user)
+        return user
+
+    def find_user_by(self, **kwargs):
+        """Finds a user in the database using a given keyword arg(s)"""
+        valid_attrs = {}
+        for k, v in kwargs.items():
+            if hasattr(User, k):
+                valid_attrs[k] = v
+            else:
+                raise InvalidRequestError
+
+        sesh = self._session
+        user = sesh.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
