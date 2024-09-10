cbb0d5778928f1ca7a0e4809a384a04c7fad7c48
Matumbai Binale <binalematumbai@gmail.com>
2024-09-10 21:28:38 +0300

    Adjust import of declarative_base

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module defining a `User` class/model"""
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
