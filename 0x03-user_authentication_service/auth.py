89d7c8a69a20a26cb4b45912e0f3cb8d81c67248
Matumbai Binale <binalematumbai@gmail.com>
2024-09-11 04:39:00 +0300

    Update a user's password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,7 +69,7 @@ class Auth:
         if user_id:
             self._db.update_user(user_id, session_id=None)
 
-    def get_reset_password_token(self, email):
+    def get_reset_password_token(self, email: str) -> str:
         """generate a reset password token"""
         try:
             user = self._db.find_user_by(email)
@@ -78,3 +78,16 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update a user's password based on a reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+        new_hashed_pwd = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_hashed_pwd,
+            reset_token=None
+        )
